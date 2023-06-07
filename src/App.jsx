df90a27e13586655a25905a9a71aeb0fa76ccda9
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-06-07 13:04:32 +0700

    feat: add Our Speciality section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/Header'
 import HeroSection from './section/HeroSection'
 import OurServiceSection from './section/OurServiceSection'
+import OurSpeciality from './section/OurSpeciality'
 import OurValueSection from './section/OurValueSection'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <HeroSection />
       <OurServiceSection />
       <OurValueSection />
+      <OurSpeciality />
     </>
   )
 }
---
+++ src/section/OurSpeciality.jsx
@@ -0,0 +1,15 @@
+import { style } from "../style"
+
+export default function OurSpeciality() {
+  return (
+    <div className="border-x-[20px] border-y-[35px] border-[#509FDD] p-10">
+      <div className={`${style.title} text-center mb-6`}>OUR SPECIALITY</div>
+      <div className="text-[#303030] text-sm leading-[20px] font-light text-center mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex ullam repellat alias officiis optio fugit tenetur nemo deserunt mollitia quos vel itaque blanditiis, placeat iure sapiente, repellendus omnis quibusdam, doloribus labore ea atque accusantium. Veritatis magnam deleniti iste totam delectus, quae, sint autem animi quasi perferendis, vitae recusandae in quisquam. Voluptas suscipit non quibusdam quae ratione hic sit autem. Accusamus!</div>
+      <div>
+        <img src="/speciality-speed.svg" alt="speed-image" className="m-auto"/>
+        <div className="text-center font-medium">Seep Improvment</div>
+      </div>
+      <div className="text-[#A7A7A7] text-sm leading-[24px] mt-10 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod libero vel leo auctor, in venenatis nulla consequat. Sed commodo nunc sit amet congue aliquam.</div>
+    </div>
+  )
+}
