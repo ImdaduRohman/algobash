cfe0ec6ddf9f13a7f2f660097dc9693d41da9075
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-06-07 10:27:02 +0700

    feat: add Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center container lg:px-28 py-8">
+      <img src="/logo.svg" alt="logo" />
+      <div>Nav Link (on going)</div>
+    </div>
+  )
+}
