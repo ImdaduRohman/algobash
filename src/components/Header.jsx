5518289a6e636f2d2a08788ab20281e96b719f11
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-06-07 15:11:57 +0700

    fix: update Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 export default function Header() {
   return (
-    <div className="flex justify-between items-center container px-8 lg:px-28 py-7">
-      <img src="/logo.svg" alt="logo" />
-      <div className="font-medium">Nav Link (on going)</div>
+    <div className="flex justify-between items-center container px-8 lg:px-28 py-5">
+      <a href="https://algobas-test-imdadu.netlify.app/" className="cursor-pointer">
+        <img src="/logo.svg" alt="logo"/>
+      </a>
+      <button onClick={() => alert('coming soon')}>
+        <img src="/burger.svg" alt="burger"/>
+      </button>
     </div>
   )
 }
