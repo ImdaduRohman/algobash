32b4787d9fd6b5b9b19a350afa18d1cfd3e77961
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-06-07 15:50:57 +0700

    fix: update padding on hero and our value section

--- src/section/HeroSection.jsx
+++ src/section/HeroSection.jsx
@@ -1,7 +1,7 @@
 export default function HeroSection() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
-      <div className="bg-[#4097DB] flex flex-col justify-center gap-16 p-10 order-last md:order-first">
+      <div className="bg-[#4097DB] flex flex-col justify-center gap-16 p-10 md:p-16 order-last md:order-first">
         <div className="text-4xl leading-[34px] font-medium text-white">DiscoverYour Wonder</div>
         <div className="text-base text-white font-normal tracking-[0.01]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
       </div>
--- src/section/OurValueSection.jsx
+++ src/section/OurValueSection.jsx
@@ -1,7 +1,7 @@
 import { style } from "../style"
 export default function OurValueSection() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 ml-16 py-16" id="ourValues">
+    <div className="grid grid-cols-1 md:grid-cols-2 ml-10 md:ml-16 py-10 md:py-16" id="ourValues">
       <div>
         <div className={`${style.title} mb-6`}>Our Core Values</div>
         <div className={style.content}>Ridiculus laoreet libero pretium et, sit vel elementum convallis fames. Sit suspendisse etiam eget egestas. Aliquet odio et lectus etiam sit.In mauris rutrum ac ut volutpat, ornare nibh diam. Montes, vitae, nec amet enim.</div>
