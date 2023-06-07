4721110dd481c84e69db80903e1999c53567be7b
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-06-07 16:09:06 +0700

    fix: edit deploy link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex justify-between items-center container px-8 lg:px-28 py-5">
-      <a href="https://algobas-test-imdadu.netlify.app/" className="cursor-pointer">
+      <a href="https://algobash-test-imdadu.netlify.app/" className="cursor-pointer">
         <img src="/logo.svg" alt="logo"/>
       </a>
       <button onClick={() => alert('coming soon')}>
